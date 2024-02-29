727c2571974c7589a4427d0c1346a58a648b519c
jimena <herrerajimena01@gmail.com>
2024-02-29 13:11:40 -0300

    se completo la creacion del producto

--- src/Components/Pages/Administracion.jsx
+++ src/Components/Pages/Administracion.jsx
@@ -1,9 +1,15 @@
-import CrearProducto from "../sections/CrearProducto";
+import ListadoProductos from "../sections/ListadoProductos";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const Administracion = () => {
+    const navigate=useNavigate();
     return (
         <div>
-            <CrearProducto></CrearProducto>
+            <div className=" container my-3 py-3">
+                <Button variant="primary" onClick={()=>{navigate("/crear-producto")}} >Crear Producto</Button>
+            </div>
+            <ListadoProductos></ListadoProductos>
         </div>
     );
 };
--- src/Components/sections/CrearProducto.jsx
+++ src/Components/sections/CrearProducto.jsx
@@ -5,24 +5,33 @@ import Form from 'react-bootstrap/Form';
 import clsx from 'clsx';
 import * as Yup from "yup";
 import { useFormik } from 'formik';
+import Swal from 'sweetalert2'
+import { useNavigate } from 'react-router-dom';
+
 
 const CrearProducto = () => {
     //los productos van a tener las siguiengtes propiedades:titulo, descripcion, categoria, ademas va a tener un identificador unico;
 // const[title, setTitle]= useState("");
 // const[description, setDescription]= useState("");
 // const[category, setCategory]= useState("");
+
+//UTILIZAMOS LA VARIABLE DE ENTORNO
+const API=import.meta.env.VITE_API;
+//UTILIZAMOS USENAVIGATE DE REACT-ROUTER-DOM
+const navigate=useNavigate();
+
 const ProductoSchema = Yup.object().shape({
     title: Yup.string().min(4, "minimo 4 caracteres").max(20, "maximo 20 caracteres").required("El titulo es requerido"),
-    description: Yup.string().min(4).max(200).required("la descripcion es requerida"),
-    category: Yup.string().required("la categoria es requerida")
+    description: Yup.string().min(4, "minimo 4 caracteres").max(200, "maximo 200 caracteres").required("La descripcion es requerida"),
+    category: Yup.string().required("La categoria es requerida")
 });
-
 const initialValues={
     title:"",
     description:"",
     category:"",
 };
 
+//Inicio conf formik
 const formik= useFormik({
      initialValues,
      validationSchema:ProductoSchema,
@@ -30,8 +39,44 @@ const formik= useFormik({
      validateOnChange: true,
      onSubmit: (values)=>{
         console.log("values de Formik-->",values);
+        Swal.fire({
+            title: "Estas seguro de guardar este producto?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Guardar"
+          }).then(async(result) => {
+            if (result.isConfirmed) {
+                try {
+                    const response= await fetch(`${API}/productos`,{
+                        method: "POST",
+                        headers:{
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(values)
+                    });
+                    //console.log("Response--",response);
+                    //console.log(response.status);
+                    if (response.status===201) {
+                        formik.resetForm();           
+                    Swal.fire({
+                     title: "Exito!",
+                     text: "Se creo un nuevo producto",
+                     icon: "success"
+                    });
+                    }
+                } catch (error) {
+                    console.log("ERROR--",error);
+                }
+
+            }
+          });
+        
+   
      }
 });
+//fin config formik
 
 /*const handleSubmit=(e)=>{
     e.preventDefault();
@@ -44,6 +89,7 @@ console.log("el nuevo producto",nuevoProducto);
 }*/
     return (
         <div className='container my-3 py-3'>
+            <Button variant='secondary' onClick={()=>navigate(-1)} >Atras</Button>
             <div className='text-center'><h2>Crear Productos</h2></div>
             <Form onSubmit={formik.handleSubmit}>
       <Form.Group className="mb-3" controlId="title">
@@ -74,7 +120,20 @@ console.log("el nuevo producto",nuevoProducto);
         // value={description} onChange={(e)=>{
         //     setDescription(e.currentTarget.value);
         // }}
+        name='description'
+        {...formik.getFieldProps("description")}
+        className={clsx("form-control",{ 
+        "is-invalid": formik.touched.description && formik.errors.description
+        },
+        {
+        "is-valid": formik.touched.description && !formik.errors.description
+        }  )}
          />
+         {formik.touched.description && formik.errors.description && (
+            <div className='mt-2 text-danger fw-bolder'>
+                <span role='alert'>{formik.errors.description}</span>
+            </div>
+        )}
       </Form.Group>
       <Form.Group className="mb-3" controlId="category">
       <Form.Label>Categoria</Form.Label>
@@ -87,13 +146,26 @@ console.log("el nuevo producto",nuevoProducto);
     //       "is-valid": validarCategoria (category)
     //     },{
     //       "is-invalid": !validarCategoria(category)
-    //     })}                      
+    //     })}    
+    name='category'
+        {...formik.getFieldProps("category")}
+        className={clsx("form-control",{ 
+        "is-invalid": formik.touched.category && formik.errors.category
+        },
+        {
+        "is-valid": formik.touched.category && !formik.errors.category
+        }  )}                  
     >
       <option value="">Seleccione una categoria</option>
       <option value="Bebidas">Bebidas</option>
       <option value="Alimentos">Alimentos</option>
       <option value="Limpieza">Limpieza</option>
     </Form.Select>
+    {formik.touched.category && formik.errors.category && (
+            <div className='mt-2 text-danger fw-bolder'>
+                <span role='alert'>{formik.errors.category}</span>
+            </div>
+        )}
     </Form.Group>
       <Button variant="primary" type="submit">
         Guardar
