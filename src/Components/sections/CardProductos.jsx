46655e7d9664205bf22cf3729f6f0424183e71b9
jimena <herrerajimena01@gmail.com>
2024-02-29 22:09:57 -0300

    se corrijio el estilo de la card de productos

--- src/Components/sections/CardProductos.jsx
+++ src/Components/sections/CardProductos.jsx
@@ -9,8 +9,8 @@ const CardProductos = ({producto}) => {
       <Card.Body>
         <Card.Title>{producto.title}</Card.Title>
         <Card.Text>
-         <span className='mb-2'>{producto.description}</span> 
-         <span className='fs-4'>{producto.category}</span>
+         <span className='mb-2 d-block'>{producto.description}</span> 
+         <span className='fs-4 d-block'>{producto.category}</span>
         </Card.Text>
         <Button variant="primary">Ver mas</Button>
       </Card.Body>
